cd292a84df613af9242c90fda7308d2aa5a3b466
Haricharan5543 <contactharicharan@gmail.com>
2024-10-26 06:23:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TenthMarksMemo from './components/TenthMarksMemo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TenthMarksMemo></TenthMarksMemo>
+      
+    </div>
+  );
+}
+
+export default App;
